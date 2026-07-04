c01aad76c1042b76b0b72c8f382ae9d000230434
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-04 07:30:23 +0000

    Jul 4, 2026, 7:30 AM

--- L01_redCircle.js
+++ L01_redCircle.js
@@ -6,9 +6,10 @@ function setup(){
 function draw(){
     fill(255, 0, 0);
     ellipse(300,200,50,50);
-        fill(0,0,255);
-        ellipse(300,200,50,50);
 
-    fill(0,255,0)
-    
+    fill(0,0,255);
+    ellipse(300,200,50,50);
+
+    fill(0,255,0);
+    ellipse(300,200,50,50);
 }
