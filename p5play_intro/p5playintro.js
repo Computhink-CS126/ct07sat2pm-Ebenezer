7907fec3bfe65bd468c5469b98296c387647625f
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 06:18:20 +0000

    Jul 11, 2026, 6:18 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,12 +3,7 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
 
-  // Basic shape testing
-  // write your codes here
- 
-  // End Basic shape testing
-
-   // Create a bouncing ball sprite
+  
    // write your codes here
 
 }
