8c8a2898aba200c725119e0f389d0541a6f1f0c1
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 07:19:32 +0000

    Jul 11, 2026, 7:19 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -33,7 +33,7 @@ function setup() {
   // circle(165, 270, 25);
   // circle(235, 270, 25);
   // ellipse(200, 335, 50, 25)
-  rect(100, 30, 180, 160);
+  rect(100, 130, 200, 160);
 }
 
 function draw() {
