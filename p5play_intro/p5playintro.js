563ed251a47ff94166380043de6762fb097bd54e
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 07:09:57 +0000

    Jul 11, 2026, 7:09 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -28,8 +28,8 @@ function setup() {
   // ellipse(200, 300, 20, 50);
   // ellipse(200, 300, 20, 5);
   circle(200, 300, 140);
-  circle(130, 370, 40);
-  circle(270, 370, 40);
+  circle(130, 270, 40);
+  circle(270, 240, 40);
 }
 
 function draw() {
