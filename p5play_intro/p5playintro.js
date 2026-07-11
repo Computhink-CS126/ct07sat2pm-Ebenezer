52b8cb0015b2a71b1244f954567d0907d5e5af9e
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 06:51:08 +0000

    Jul 11, 2026, 6:51 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -38,5 +38,7 @@ function draw() {
     if (ball.y < 0 + ball.diameter/2 || ball.y > height - ball.diameter / 2) {
     ball.vel.y *= -1 
   }
-  box.x = mouseX
+  box.x = mouseX;
+  box.y = mouseY;
+
 }
