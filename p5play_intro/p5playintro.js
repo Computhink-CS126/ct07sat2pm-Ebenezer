0e5db0cdd30c08fb73d4a0276acc69403de3323f
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 06:57:11 +0000

    Jul 11, 2026, 6:57 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -6,23 +6,23 @@ function setup() {
   // Set up the canvas
   new Canvas(400,600);
   background(250); //background color
-  ball = new Sprite();
-  fill("skyblue");
-  circle(25,25,50);
-  fill("cyan");
-  rect(50,50, 30, 50);
-   // write your codes here
-  ball = new Sprite();
-  ball.x = 360;
-  ball.y = 30;
-  ball.diameter = 40;
-  ball.color = 'red'
-  box = new Sprite();
-  box.x = 100
-  box.y = 100
-  box.w = 50
-  box.h = 75
-  box.color = 'purple';
+  // ball = new Sprite();
+  // fill("skyblue");
+  // circle(25,25,50);
+  // fill("cyan");
+  // rect(50,50, 30, 50);
+  //  // write your codes here
+  // ball = new Sprite();
+  // ball.x = 360;
+  // ball.y = 30;
+  // ball.diameter = 40;
+  // ball.color = 'red'
+  // box = new Sprite();
+  // box.x = 100
+  // box.y = 100
+  // box.w = 50
+  // box.h = 75
+  // box.color = 'purple';
 }
 
 function draw() {
