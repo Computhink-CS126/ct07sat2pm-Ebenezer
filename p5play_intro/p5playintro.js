01fc8625e97372338dee98330192a81e13e6d65d
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-11 06:44:31 +0000

    Jul 11, 2026, 6:44 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -6,7 +6,8 @@ function setup() {
   // Set up the canvas
   new Canvas(400,600);
   background(250); //background color
-  fill(0)
+  fill(0);
+  textSize(16);
   ball = new Sprite();
   fill("skyblue");
   circle(25,25,50);
