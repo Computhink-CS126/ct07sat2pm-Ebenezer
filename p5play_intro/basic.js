f3b878b95ced998d4aaf1c0019f118b54318e101
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-18 06:45:03 +0000

    Jul 18, 2026, 6:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -5,13 +5,14 @@ function setup() {
   // write your codes here
 new Canvas(800, 400);
 background(220);
+texstSize(16)
   // --- Exercise: Area of Triangle ---
   // write your codes here
 area = 0.5 * base * height;
 console.log("The area of the triangle:", area);
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
-
+text
   // --- Exercise: Age category classification ---
   // write your codes here
 
