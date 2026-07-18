20020fe7ddd125f33ae630121880e781d655840a
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-18 06:51:58 +0000

    Jul 18, 2026, 6:51 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -20,7 +20,8 @@ textSize(16)
   for (let i = 2; 1 > 20 ; i + 2) {
     number = number + i
   }
-  console.log()
+  console.log(number)
+  text
   // --- Exercise: Age category classification ---
   // write your codes here
 
