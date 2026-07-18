47337d7280208bc138164ec12a1701eff2094f5f
Computhink-CS126 <cs126@computhink.com.sg>
2026-07-18 07:31:42 +0000

    Jul 18, 2026, 7:31 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -45,7 +45,7 @@ textSize(16)
   // }
   let groceries = ["apple", "bread", "milk"]
   console.log(groceries)
-  
+  groceries.push("orange")
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
 
