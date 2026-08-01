23c305601d8226f6b0ecf181c56ca9f67dd1a57a
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-01 06:43:01 +0000

    Aug 1, 2026, 6:43 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -41,5 +41,7 @@ function draw(){
     text('vel.y: ' + bird.vel.y.toFixed(2), 10, 20);
     text('isMoving: ' + bird.isMoving, 10, 40);
     text('sleeping: ' + bird.sleeping, 10, 60)
-
+    if (bird.vel.y , -1) {
+        bird
+    }
 }
