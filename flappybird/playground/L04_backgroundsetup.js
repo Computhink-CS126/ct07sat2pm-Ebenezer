b19b8f278acc84de69cbfc804b99cdec064eff2c
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-01 06:39:37 +0000

    Aug 1, 2026, 6:39 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -5,7 +5,7 @@ function preload() {
     midflapimg = loadImage('assets/redbird-midflap.png');
     bg = loadImage('assets/background-night.png');
     base = loadImage('assets/base.png');
-    flapUpImg = loadImage('asset')
+    flapUpImg = loadImage('asset/yellowbird-upflap')
 }
 function setup(){
     createCanvas(400,600);
