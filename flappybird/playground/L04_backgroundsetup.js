a4f5383e39490e37e267104a9949ac72a9956ea5
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-08 06:27:48 +0000

    Aug 8, 2026, 6:27 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -71,7 +71,7 @@ function draw(){
     if (frameCount === 1) {
         spawnPipePair();
     }
-    draw(width/2,)
+    draw(width/2, 20, score, 24)
 }
 function spawnPipePair(){
     let gap = 50;
