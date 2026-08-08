928ab2a017ec0a1be6e09271acf80e431912ee50
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-08 06:40:14 +0000

    Aug 8, 2026, 6:40 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -84,7 +84,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
         let digitSprite = new scoreDigits.Sprite(xPos, y, digitWidth,digitHeight);
         digitSprite.img = numberImages[digit];
     }
-    moveGroup
+    moveGroup(scoreDigits)
 }
 function spawnPipePair(){
     let gap = 50;
