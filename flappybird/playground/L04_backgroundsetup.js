ffaf27e774203c8aa7661e9be2c03b76731d4238
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-15 06:15:28 +0000

    Aug 15, 2026, 6:15 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -49,7 +49,7 @@ function setup(){
     scoreDigits = new Group()
     scoreDigits.collider = 'none'
     scoreDigits.layer = 1000;
-    startScreenLabel = new Sprite
+    startScreenLabel = new Sprite(width/2, height/2)
 }
 function draw(){
     image(bg,0,0,width,height);
