cf4607cf3a43b7f243838f0e7050fec1a72c9b1a
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-15 07:00:45 +0000

    Aug 15, 2026, 7:00 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -27,7 +27,7 @@ numberImages[i] = loadImage('assets/' + i +'.png');
 }
 startScreenImg = loadImage('assets/message.png');
 flapSound = createAudio('assets/sfx_wing.mp3');
-pointSound = createAudio
+pointSound = createAudio('assets/sfx_')
 }
 
 function setup(){
