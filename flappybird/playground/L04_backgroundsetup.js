b1d53dd3855cf7d127a8b97d2d516b7869a61290
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-15 06:47:53 +0000

    Aug 15, 2026, 6:47 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -153,11 +153,11 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
 function spawnPipePair(){
     let gap = 50;
     let midY = random(250, height - 250);
-    bottomPipe = new Sprite(bird.x + 400, midY + gap / 2 + 200, 52 , 320, 'static');
+    bottomPipe = new Sprite(bird.x + 400, midY + (gap / 2) + 200, 52 , 320, 'static');
     bottomPipe.img = pipe;
     pipeGroup.add(bottomPipe);
     pipeGroup.layer = 0;
-    topPipe = new Sprite( bird.x + 400, midY + gap / 2 - 200, 52, 320, 'static');
+    topPipe = new Sprite( bird.x + 400, midY + (gap / 2 )- 200, 52, 320, 'static');
     topPipe.img = pipe;
     topPipe.rotation = 180;
     topPipe.passed=false;
