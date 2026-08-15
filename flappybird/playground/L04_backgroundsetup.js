c7a75387f9576437a2fb1271f9e1c9f430a5cac7
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-15 06:19:07 +0000

    Aug 15, 2026, 6:19 AM

--- flappybird/playground/L04_backgroundsetup.js
+++ flappybird/playground/L04_backgroundsetup.js
@@ -11,7 +11,7 @@ let gameoverImg;
 let gameoverLabel;
 let startScreenLabel;
 let startScreenImg;
-let 
+let startGame = false
 function preload() {
     midflapimg = loadImage('assets/redbird-midflap.png');
     bg = loadImage('assets/background-night.png');
