6c31cedd6af1323d05a26017205e371ed9193528
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-22 07:30:13 +0000

    Aug 22, 2026, 7:30 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -30,6 +30,7 @@ function spawnFruit(){
     let fruitData = random(fruitTypes);
     let randomX = random(300,500);
     let fruit = new fruitGroup.Sprite(randomX, height+20, 40);
-    fruit.type = 
+    fruit.type = fruitData;
+    
     
 }
