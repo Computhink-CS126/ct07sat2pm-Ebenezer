bd2e14a885385f1bd33627357af44bcb2bd51acc
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-22 07:01:06 +0000

    Aug 22, 2026, 7:01 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -1,9 +1,13 @@
 let backdrop;
 let bS;
+
 function preload() {
     backdrop = loadImage('assets/dojobackground.png');
 }
+
 function setup(){
     newCanvas(800,600);
     world.gravity.y = 10;
 }
+
+function draw()
