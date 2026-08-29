a681dabb6b4778360abf05d5ca8be6c8f8b1b1cf
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-29 07:26:27 +0000

    Aug 29, 2026, 7:26 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -63,7 +63,7 @@ function sliceFruit(){
         fruit.remove();
         splitFruit(fx, fy, fruit.type);
         break;
-    }
+    }}
 }
 function splitFruit(x, y, fruitData){
     let left = new fruitHalves.Sprite(x - 10, y, 40, 40);
