bd48169b114c62d62a9bdc5a79fabcad21858edc
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-29 06:36:50 +0000

    Aug 29, 2026, 6:36 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -12,6 +12,7 @@ function preload() {
     let watermelon = {
         whole: loadImage('assets/watermelonwhole.png'),
         half1: loadImage('assets/watermelonhalf.png'),
+        half2: loadImage('assets/watermelonhalf.png'),
     };
     fruitTypes = [peach,watermelon];
 }
