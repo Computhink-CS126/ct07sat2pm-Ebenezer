c5c65a8b1750542465154cee1098b065aa4f3539
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-29 06:14:35 +0000

    Aug 29, 2026, 6:14 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -28,7 +28,7 @@ function draw(){
         trail = new Sprite(mouse.x, mouse.y, 7);
         trail.collider = 'none';
         trail.color = 'red';
-        trail.life;
+        trail.life = 10;
     }
 }
 
