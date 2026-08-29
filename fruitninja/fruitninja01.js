00d1ced6a93d4f14d931f79e000e96e03df1ceb0
Computhink-CS126 <cs126@computhink.com.sg>
2026-08-29 06:44:30 +0000

    Aug 29, 2026, 6:44 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -58,6 +58,7 @@ function sliceFruit(){
     let d = dist(mouse.x, mouse.y, fruit.x, fruit.y);
     if (d< ((fruit.d/2) +5)) {
         fruit.sliced = true;
+        
         fruit.remove();
         break;
     }
