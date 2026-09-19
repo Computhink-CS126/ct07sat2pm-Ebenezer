7bad746140517ce3df2a5d2a489216ebb5a53b47
Computhink-CS126 <cs126@computhink.com.sg>
2026-09-19 07:14:56 +0000

    Sep 19, 2026, 7:14 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -6,7 +6,8 @@ let fruitHalves;
 let bombarray = []
 let bombGroup;
 let exploded = false;
-let explosionX, explosionY
+let explosionX, explosionY;
+let explosionTimer
 function preload() {
     backdrop = loadImage('assets/dojobackground.png');
     let peach = {
