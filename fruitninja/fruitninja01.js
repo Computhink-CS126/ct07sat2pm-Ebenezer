4ff430af0944bae0426cb0c1707010dfa972cf61
Computhink-CS126 <cs126@computhink.com.sg>
2026-09-26 06:59:22 +0000

    Sep 26, 2026, 6:59 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -49,7 +49,7 @@ function draw(){
         textSize(48);
         text('Fruit Ninja', width/2, height / 2 - 40);
         textSize(24);
-        text('Press SPACE or Click to Start', width/2)
+        text('Press SPACE or Click to Start', width/2, height/'2 + 20)')
     }
     if (frameCount % 120 === 0){
         spawnFruit();
